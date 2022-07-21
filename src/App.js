96a6dc982158e53154157c2fcf24bbfc3b52988d
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-07-21 12:02:30 +0530

    add to cart design done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import './App.css';
-import User from './User';
-import Home from './components/Home';
-
+import HomeContainer from './containers/HomeContainer';
 function App() {
-  const emp=[
-    {
-      name:"ketan",
-      age:24,
-      email:"ketan@gmail.com"
-    },
-    {
-      name:"Yashwant",
-      age:24,
-      email:"yashwant@gmail.com"
-    }
-  ]
+  
   return (
     <div className="App">
-      <Home/>
-      <h1>App Component</h1>
-      <User data={emp}/>
+      <HomeContainer/>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,27 @@
+import React from 'react'
+import pic1 from '../img/iphone.png';
+import pic2 from '../img/iphone6snew.png';
+import pic3 from '../img/basketcart.png';
 const Home=()=>{
     return(
         <div>
+            <div className='add-to-cart'>
+                <img src={pic3}/>
+            </div>
             <h1>Home Page</h1>
+            <div className='cart-wrapper'>
+                <div className='img-wrapper item'>
+                    <img src={pic2} alt='best iphone'/>
+                </div>
+                <div className='text-wrapper item'>
+                    <span>I-Phone</span>
+                    <span>Price:$1000.00</span>
+
+                </div>
+                <div className='btn-wrapper item'>
+                    <button>Add to cart</button>
+                </div>
+            </div>
         </div>
     );
 }
